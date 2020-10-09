7ab34f6f2954b5600d0a226f8dc6c4d424b717e7
ReddyyZ <arthurc.oli.whitehat@gmail.com>
2020-10-09 19:55:35 -0300

    Updating Reading Time Calculator script

--- assets/js/calculate-reading-time.js
+++ assets/js/calculate-reading-time.js
@@ -1,13 +1,27 @@
 function ReadingTime(words_per_minute){
     const ps = document.querySelectorAll("p");
-    console.log(ps);
+    const pres = document.querySelectorAll("pre");
 
-    var count = 0;
+    var count_p = 0;
+    var count_pre = 0;
 
     for (var i =0; i < ps.length; i++){
-        count += ps[i].innerHTML.split(" ").length;
+        count_p += ps[i].innerHTML.split(" ").length;
+    }
+    for (var i =0; i < pres.length; i++){
+        count_pre += pres[i].innerHTML.split(" ").length;
     }
 
+    p_minutes = make_count(count_p,words_per_minute);
+    pre_minutes = make_count(count_pre,words_per_minute);
+
+    minutes = p_minutes + pre_minutes;
+
+    console.log("Reading time:", minutes, minutes == 1 ? "minute" : "minutes");
+    return minutes;
+}
+
+function make_count(count,words_per_minute){
     x = count / words_per_minute;
     x = String(x).split('.');
     
@@ -18,7 +32,5 @@ function ReadingTime(words_per_minute){
     y = Math.round(y);
     y < 30 ? y = 0 : y = 1
     minutes = parseInt(minutes) + y;
-
-    console.log("Reading time:", minutes, minutes == 1 ? "minute" : "minutes");
     return minutes;
 }
