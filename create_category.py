e13b192082eb121ebd1042523deb248c3075eb7a
ReddyyZ <arthurc.oli.whitehat@gmail.com>
2020-10-15 14:35:56 -0300

    Some updates

--- create_category.py
+++ create_category.py
@@ -43,7 +43,7 @@ tag: "{{tagg}}"
 title: "{{titlee}}"
 ---
 
-<h1>Posts with "{{page.tag}}" tag</h1>
+<h1>Posts with "{{{{page.tag}}}}" tag</h1>
     
 <ul>
 {{% for post in site.posts %}}
@@ -53,12 +53,12 @@ title: "{{titlee}}"
     <a href="{{{{ post.url }}}}">{{{{ post.title }}}}</a>
     <br>
     {{% if post.description %}}
-    {{ post.description }}
+    {{{{ post.description }}}}
     {{% else %}}
-    {{ post.excerpt }}
+    {{{{ post.excerpt }}}}
     {{% endif %}}
 
-    <p>Author: {{{{post.author{{{{</p>
+    <p>Author: {{{{post.author}}}}</p>
     <p>Categories: {{% for category in post.categories %}} <a class="tag" href="/category/{{{{category}}}}">{{{{ category }}}}</a> {{% endfor %}}</p>
     <p>Tags: {{% for tag in post.tags %}} <a class="tag" href="/tag/{{{{tag}}}}">{{{{ tag }}}}</a> {{% endfor %}}</p>
     <p>Date: {{{{post.date | date: "%-d %B %Y"}}}}</p>
